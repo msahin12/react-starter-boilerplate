d632223700662162f441579f963c53e6557d5387
MS <gakeko2018@gmail.com>
2020-12-23 21:40:47 +0200

    Fix providers filename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root";
-import Providers from "./Providers";
+import Providers from "./providers";
 import MainRouter from "SRC/router";
 
 const render = (Component) => {
